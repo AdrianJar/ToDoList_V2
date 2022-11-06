1f727f4d01aca31a0ec2305ac91c5adf6b026449
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-11-06 18:46:51 +0100

    Added makr all done button and added styles to it

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,6 @@
 {
     let tasks = [];
-    let hideDoneTask = false;
+    let hideDoneTasks = false;
 
     const addNewTask = (newTaskContent) => {
         tasks = [
@@ -29,11 +29,6 @@
         ];
 
         render();
-    };
-
-    const clearInput = () => {
-        const clearInput = document.querySelector(".js-newTask");
-        clearInput.value = "";
     };
 
     const bindRemoveEvents = () => {
@@ -75,27 +70,79 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const setAllTasksDone = () => {
+        tasks = tasks.map((task) => ({
+            ...task,
+            done: true,
+        }));
+
+        render();
+    };
+
+    const toggleHideDoneTasks = () => {
+        hideDoneTasks = !hideDoneTasks;
+
+        render();
+    };
+
+    const bindButtonsEvents = () => {
+        const setAllTasksDoneButton = document.querySelector(".js-setAllTasksDone");
+
+        if (setAllTasksDoneButton) {
+            setAllTasksDoneButton.addEventListener("click", () => {
+                setAllTasksDone();
+            });
+        }
+
+        const toggleHideDoneTasksButton = document.querySelector(".js-hideDoneTask");
+
+        if (toggleHideDoneTasksButton) {
+            toggleHideDoneTasksButton.addEventListener("click", () => {
+                toggleHideDoneTasks();
+            });
+        }
+    };
+
+    const renderButtons = () => {
+        let htmlButtonsString = "";
+
+        if (!tasks.length) {
+            return;
+        }
+        htmlButtonsString += `
+    <button class="js-hideDoneTask buttons__button"> ${hideDoneTasks ? "Pokaż " : "Ukryj "
+            }ukończone</button>
+    <button ${tasks.every(({ done }) => done) ? "disabled" : ""}
+            class="js-setAllTasksDone buttons__button">
+              Ukończ wszystkie
+            </button>`;
+
+
+        document.querySelector(".js-buttonEvents").innerHTML = htmlButtonsString;
+    };
+
+
+
     const render = () => {
         renderTasks();
-
+        renderButtons();
 
         bindRemoveEvents();
         bindDoneEvents();
+        bindButtonsEvents();
     };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
+        const newTaskElement = document.querySelector(".js-newTask");
+        const newTask = newTaskElement.value.trim();
 
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-        if (newTaskContent === "") {
-            return;
-        }
-        {
-            addNewTask(newTaskContent);
+        if (newTask !== "") {
+            addNewTask(newTask);
+            newTaskElement.value = "";
         }
 
-        clearInput();
+        newTaskElement.focus();
     };
 
     const init = () => {
@@ -104,7 +151,6 @@
         const form = document.querySelector(".js-form");
 
         form.addEventListener("submit", onFormSubmit);
-
     };
 
     init();
